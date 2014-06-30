cfe200e95e272d69567c7e6b3cb11260a7c21e31
Alex Vernacchia <avernacchia@exacttarget.com>
2014-06-30 14:03:23 -0400

    clean up, renaming, etc.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,20 @@
 module.exports = function( grunt ) {
-  'use strict';
+	'use strict';
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      }
-      , all: [
-        'Gruntfile.js'
-        , 'lib/**/*.js'
-        , 'test/app/js'
-      ]
-    }
-  });
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			}
+			, all: [
+				'Gruntfile.js'
+				, 'lib/**/*.js'
+				, 'test/**/*.js'
+			]
+		}
+	});
 
-  grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-  grunt.registerTask( 'default', [ 'jshint' ] );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.registerTask( 'default', [ 'jshint' ] );
 };
