a9941290e91a0af0ed74ee75b15eaadc4c989e46
csarov <csarov@salesforce.com>
2018-09-27 16:04:50 +0300

    added tests for proxy support

---
+++ test/specs/fn-proxy-support.js
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root  or https://opensource.org/licenses/BSD-3-Clause
+ */
+'use strict';
+const expect = require('chai').expect;
+const FuelRest = require('../../lib/fuel-rest');
+const routes = require('../config').routes;
+const http = require('http');
+
+const port = 4550;
+const proxyPort = 8888;
+const localhost = `http://127.0.0.1:${port}`;
+const proxyResponseBody = 'Hello Node JS Server Response';
+const requestOptions = {
+	method: 'GET',
+	uri: routes.get
+};
+const server = http.createServer(function (request, response) {
+	response.writeHead(200, {'Content-Type': 'application/json'});
+	response.write(proxyResponseBody);
+	response.end();
+});
+const accessToken = 'testForRest';
+const expiration = 111111111111;
+
+describe('Proxy support', function () {
+
+	let restClient, initOptions;
+
+	before(done => server.listen(proxyPort, done));
+
+	beforeEach(() => {
+		initOptions = {
+			auth: {
+				clientId: 'testing',
+				clientSecret: 'testing'
+			},
+			restEndpoint: localhost,
+			proxy: {
+				host: '127.0.0.1',
+				protocol: 'http:'
+			}
+		};
+	});
+
+	it('should respond the proxyResponseBody if proxy option passed correctly', done => {
+		initOptions.proxy.port = proxyPort;
+		restClient = new FuelRest(initOptions);
+		restClient.AuthClient.accessToken = accessToken;
+		restClient.AuthClient.expiration = expiration;
+		restClient.apiRequest(requestOptions, (err, data) => {
+			expect(data.body).to.equal(proxyResponseBody);
+			done();
+		});
+	});
+
+	it('should error if proxy option passed incorrectly', done => {
+		initOptions.proxy.port = 1234;
+		restClient = new FuelRest(initOptions);
+		restClient.AuthClient.accessToken = accessToken;
+		restClient.AuthClient.expiration = expiration;
+		restClient.apiRequest(requestOptions, (err) => {
+			expect(err.code).to.equal('ECONNREFUSED');
+			done();
+		});
+	});
+
+	after(() => server.close());
+
+});
